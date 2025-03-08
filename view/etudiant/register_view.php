63e926d61b85407d7913a38f3608978f47beb916
karimouertatani <mohamedkarim.ouertatani@esprit.tn>
2025-03-08 02:41:12 +0100

    new

--- view/etudiant/register_view.php
+++
@@ -1,8 +0,0 @@
-<form action="index.php?page=register" method="POST">
-    <input type="text" name="nom" placeholder="Nom" required>
-    <input type="text" name="prenom" placeholder="Prénom" required>
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="mot_de_passe" placeholder="Mot de passe" required>
-    <input type="date" name="date_naissance" required>
-    <button>S'inscrire</button>
-</form>
